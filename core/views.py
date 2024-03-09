ae4cad6520e3b2b2308c7a6890385cefcc85b587
pedramkarimii <pedram.9060@gmail.com>
2024-03-09 14:53:19 +0330

    :sparkles: creat search with TrigramSimilarity and Greatest

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
+from django.contrib.postgres.search import TrigramSimilarity
 from django.shortcuts import render
+from django.db.models.functions import Greatest
+from posts.models import Post
+from .forms import SearchForm
 
-# Create your views here.
+
+def search(request):
+    post = Post.objects.all()
+    form = SearchForm()
+    if request.method == 'GET':
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            cd = form.cleaned_data['search']
+            posts = post.annotate(
+                similarity=Greatest(TrigramSimilarity('name', cd),
+                                    TrigramSimilarity('surname', cd),
+                                    TrigramSimilarity('patronymic', cd)
+                                    ).filter(similarity__gt=0.1).order_by('-similarity'))
+            return render(request, 'search.html', {'posts': posts, 'form': form, })
