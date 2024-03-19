af39e5930ba7e6a94f47df055cf34e5c6c968f46
pedramkarimii <pedram.9060@gmail.com>
2024-03-19 12:57:30 +0330

    :sparkles: add logger path save in file info.log

--- core/middlewares.py
+++ core/middlewares.py
@@ -1,72 +1,41 @@
+import logging
 from django.contrib import messages
-from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+"""Initialize the logger with the current module name."""
+logger = logging.getLogger(__name__)
 
-LOGIN_EXEMPT_URLS = [
-    '/login/',
-    '/register/',
-    '/verify/',
-    '/login_verify_code/',
-    '/login_verify/',
-    '/success_login/',
-    '/resatpassword/',
-    '/resatpassword/done/',
-    '/admin/login/',
-    '/confirm/MQ/',
-    '/confirm/resatcomplete/',
-    '/',
-]
-PROFILE_EXEMPT_URLS = [
-    '/createpost/',
-    'show_post/<int:pk>/',
-    'explorer/<int:pk>/',
-    'follow/<int:user_id>/',
-    'unfollow/<str:user_id>/',
-    'like/<str:post_id>/',
-    'dislike/<str:post_id>/',
-    'post/<int:pk>/update/',
-    'post/<int:pk>/delete/',
-]
-AFTER_LOGIN_REDIRECT_URL = [
-    '/login/',
-    '/register/',
-    '/verify/',
-    '/login_verify_code/',
-    '/login_verify/',
-    '/success_login/',
-    '/resatpassword/',
-    '/resatpassword/done/',
-    '/admin/login/',
-    '/confirm/MQ/',
-    '/confirm/resatcomplete/',
+"""Define the path for the log file."""
+LOG_FILE_PATH = '/home/pedram/Desktop/project django/core/info.log'
 
-]
+"""Configure logging with the specified log file, log level, and format."""
+logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class LoginRequiredMiddleware:
+    """Middleware class to handle login requirement."""
+
     def __init__(self, get_response):
+        """Initialize the middleware."""
         self.get_response = get_response
 
     def __call__(self, request):
-        if not request.user.is_authenticated and request.path_info not in LOGIN_EXEMPT_URLS:
-            messages.warning(request, 'You should login first', extra_tags='warning')
-            return HttpResponseRedirect(reverse('login'))
-        elif request.user.is_authenticated and request.path_info in AFTER_LOGIN_REDIRECT_URL:
-            messages.warning(request, 'You should logout first', extra_tags='warning')
-            return HttpResponseRedirect(reverse('home'))
-        if request.user.is_authenticated and request.path_info not in PROFILE_EXEMPT_URLS:
-            try:
-                profile = request.user.profile # noqa
-            except ObjectDoesNotExist:
-                if request.path_info != reverse('create_profile'):
-                    messages.warning(request, 'You should create your profile first', extra_tags='warning')
-                    return HttpResponseRedirect(reverse('create_profile'))
+        """Handle each incoming request."""
+
+        logger.info(f"Request for URL: {request.path}. Method: {request.method}.")
 
         response = self.get_response(request)
 
         if response.status_code in [400, 404, 405, 406, 500, 401, 403]:
+            logger.error(
+                f"Error {response.status_code} occurred for URL: {request.path}. "
+                f"Method: {request.method}. User: {request.user}")
             messages.warning(request, 'An error occurred. Please try again.', extra_tags='error')
             return HttpResponseRedirect(reverse('home'))
+
+        logger.info(
+            f"Request for URL: {request.path}. Method: {request.method}. User: {request.user}. "
+            f"Status Code {response.status_code}")
+
         return response
