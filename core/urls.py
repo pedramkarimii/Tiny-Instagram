4e90048b99bbb801806eee8fee30677b6a332a08
pedramkarimii <pedram.9060@gmail.com>
2024-03-21 15:06:14 +0330

    :sparkles: create urls contact us, about us and home

---
+++ core/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+from core.views import ContactUs
+
+"""
+Defines URL patterns for the home, about us, and contact us pages.
+- The root URL ('') is associated with a TemplateView displaying the 'bases.html' template, serving as the home page.
+- The '/about_us/' URL pattern is associated with a TemplateView displaying the 'about_us.html' template, representing the about us page.
+- The '/contact_us/' URL pattern is associated with the ContactUs view, allowing users to access the contact us page.
+These URL patterns define the navigation structure of the website, directing users to different pages.
+"""
+
+
+urlpatterns = [
+    path("", TemplateView.as_view(template_name='base/bases.html'), name="home"),
+    path("about_us/", TemplateView.as_view(template_name='about_us/about_us.html'), name="about_us"),
+    path('contact_us/', ContactUs.as_view(), name='contact_us'),
+]
