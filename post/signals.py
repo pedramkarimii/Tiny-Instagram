5860a4a9a722eb683d131662e97639c4c86fa60f
pedramkarimii <pedram.9060@gmail.com>
2024-03-29 02:12:02 +0330

    :sparkles: add signal for image

---
+++ post/signals.py
@@ -0,0 +1,8 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Image, Post
+
+@receiver(post_save, sender=Post)
+def create_image_for_post(sender, instance, created, **kwargs):
+    if created and getattr(instance, 'image_field', None):
+        Image.objects.create(post_image=instance, images=getattr(instance, 'image_field'))
