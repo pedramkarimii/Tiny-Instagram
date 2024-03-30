fa8dbb6d5dc51d74c9505492e73e1b7d069a8720
pedramkarimii <pedram.9060@gmail.com>
2024-03-30 19:00:25 +0330

    :sparkles: add url delete comment

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from post.views import HomePostView, UpdatePostView, DeletePostView, Explorer, CreatePostView, FollowUserView, \
-    PostLikeView, PostDetailView, ReplyCommentView
+    PostLikeView, PostDetailView, ReplyCommentView, DeleteCommentView
 
 """
 Defines URL patterns for the application.
@@ -22,6 +22,7 @@ urlpatterns = [
     path('explorer/', Explorer.as_view(), name="explorer"),
     path('show_post/<int:pk>/', HomePostView.as_view(), name='show_post'),
     path('comment/<int:pk>/reply/', ReplyCommentView.as_view(), name='reply_comment'),
+    path('comment/<int:pk>/delete/', DeleteCommentView.as_view(), name='delete_comment'),
     path('post_detail/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('follow/<int:pk>/', FollowUserView.as_view(), name='follow_user'),
     path('like/<int:post_id>/', PostLikeView.as_view(), name='like_user'),
