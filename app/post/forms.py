fab960c893045da3bd31d6e803b003210c63851a
pedramkarimii <pedram.9060@gmail.com>
2024-03-31 15:13:29 +0330

    :bug: fixed widget form create post

--- app/post/forms.py
+++ app/post/forms.py
@@ -20,42 +20,21 @@ class ImageForm(forms.ModelForm):
             'images': 'Image',
         }
 
-    # def clean_images(self):
-    #     images = self.cleaned_data.get('images')
-    #     if image.size > 10485760:
-    #         raise forms.ValidationError('Image size must be less than 10MB.')
-    # def clean_post_image(self):
-    #     post_image = self.cleaned_data.get('post_image')
-    #     if post_image.user == self.request.user.profile:
-    #         raise forms.ValidationError('You cannot upload an image to yourself.')
-    #     return post_image
-    #
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     images = cleaned_data.get('images')
-    #     post_image = cleaned_data.get('owner_image')
-    #
-    #     if images and post_image:
-    #         if images.user != post_image.owner.user:
-    #             raise forms.ValidationError('You cannot upload an image to another user.')
-    #     return cleaned_data
-
 
 class UpdatePostForm(forms.ModelForm):
     """
     Form for updating a post.
     """
-    title = forms.CharField(label='Title', required=False, widget=TextInput(attrs={
+    title = forms.CharField(label='Title', required=True, widget=TextInput(attrs={
         'class': 'mt-1 mb-8 pt-2 py-2 px-4 focus:ring-indigo-500 focus:border-indigo-500 block w-full'
                  ' shadow-sm sm:text-sm border-gray-300 rounded-md'}))
     body = forms.CharField(label='Body',
-                           required=False, widget=CKEditorWidget(attrs={'class': 'mt-1 mb-4 pt-2 py-2 px-4 '
-                                                                                 'focus:ring-indigo-500 '
-                                                                                 'focus:border-indigo-500 block w-full '
-                                                                                 'shadow-sm sm:text-sm border-gray-300 '
-                                                                                 'rounded-md'}))
-    Image = forms.ImageField(label='Post Image', required=False, widget=forms.FileInput(
-        attrs={'class': 'form-control mt-1 pt-2 px-4 text-indigo-600 border-gray-300 rounded-md'}))
+                           required=True, widget=CKEditorWidget(attrs={'class': 'mt-1 mb-4 pt-2 py-2 px-4 '
+                                                                                'focus:ring-indigo-500 '
+                                                                                'focus:border-indigo-500 block w-full '
+                                                                                'shadow-sm sm:text-sm border-gray-300 '
+                                                                                'rounded-md'}))
+    Image = forms.ImageField(label='Post Image', required=True)
 
     class Meta:
         model = Post
@@ -87,7 +66,6 @@ class CreatCommentForm(forms.ModelForm):
         post_id = kwargs.pop('post_id', None)
         super().__init__(*args, **kwargs)
         if post_id:
-
             self.fields['reply'].queryset = Comment.objects.filter(post_id=post_id, is_reply=True)
 
     comments = forms.CharField(
@@ -113,14 +91,3 @@ class CreatCommentForm(forms.ModelForm):
         elif len(body) > 500:
             raise forms.ValidationError('Body must be less than 500 characters long.')
         return body
-
-    # def clean_reply(self):
-    #     reply_id = self.cleaned_data.get('reply')
-    #     if reply_id is not None:
-    #         try:
-    #             reply = Post.objects.get(pk=reply_id)
-    #         except Post.DoesNotExist:
-    #             raise forms.ValidationError('Invalid post selected.')
-    #         return reply
-    #     else:
-    #         return None
