3a1776c7b6c3fa55749868291724dd19e33c1399
pedramkarimii <pedram.9060@gmail.com>
2024-03-31 15:12:09 +0330

    :bug: import signal post and fix signal

--- app/post/apps.py
+++ app/post/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 class PostConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app.post'
+
     def ready(self):
-        pass
+        from app.post import signals
--- app/post/signals.py
+++ app/post/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Image, Post
 
+
 @receiver(post_save, sender=Post)
 def create_image_for_post(sender, instance, created, **kwargs):
     if created and getattr(instance, 'image_field', None):
