f68c64f15ce33538066f6c22139aaab3971ba4d6
pedramkarimii <pedram.9060@gmail.com>
2024-04-02 01:50:40 +0330

    :memo: fixe line

--- app/account/tests.py
+++ app/account/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import Profile, OptCode
+
 User = get_user_model()
 
 
@@ -9,15 +10,13 @@ class ModelsTestCase(TestCase):
 
     def setUp(self):
         """Set up initial data for tests."""
-        # Create a user object
         self.user = User.objects.create(username='pedramkarimi', email='pedram.9060@gmail.com',
                                         phone_number='09128355747')
-        # Create a profile object associated with the user
         self.profile = Profile.objects.create(user=self.user, full_name='Pedram Karimi', name='pedram',
                                               last_name='karimi',
                                               gender='Female', age=30, bio='Hi',
-                                              profile_picture='profile_picture/2024/03/17/d63d11b6-2ebc-46ea-9384-0a776f97e278_1pMrWIo.jpeg')
-        # Create an OptCode object
+                                              profile_picture='profile_picture/2024/03/17/d63d11b6'
+                                                              '-2ebc-46ea-9384-0a776f97e278_1pMrWIo.jpeg')
         self.opt_code = OptCode.objects.create(code=1234, phone_number='09128355747')
 
     def test_user_creation(self):
@@ -38,6 +37,5 @@ class ModelsTestCase(TestCase):
 
     def test_opt_code_phone_number_uniqueness(self):
         """Test uniqueness of phone numbers in OptCode."""
-        # Creating another OptCode object with the same phone number should raise an exception
         with self.assertRaises(Exception):
             OptCode.objects.create(code=1234, phone_number='09128355747')
