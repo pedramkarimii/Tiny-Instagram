7ac8638ede4e99c3b180edd13d3c451f3e08f7a4
pedramkarimii <pedram@9060.com>
2024-07-26 23:18:23 +0330

    :ambulance: Replace hardcoded settings with `decouple.config` for environment variable management Update middleware and caching setup for debug and production modes Configure email backend settings for debug and production Refactore INSTALLED_APPS to dynamically include applications based on the environment Improve file and cache handling based on the environment

--- config/settings.py
+++ config/settings.py
@@ -1,88 +1,77 @@
-from pathlib import Path
 import os
+from pathlib import Path
+from datetime import timedelta
+from decouple import config  # noqa
 
+USE_TZ = True
+USE_I18N = True
+LANGUAGE_CODE = "en-us"
+ROOT_URLCONF = "config.urls"
+AUTH_USER_MODEL = 'account.User'
+WSGI_APPLICATION = "config.wsgi.application"
+TIME_ZONE = config("TIME_ZONE", default="UTC")
+DEBUG = config("DEBUG", cast=bool, default=True)
 BASE_DIR = Path(__file__).resolve().parent.parent
+APP_DIR = BASE_DIR / "apps"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+SECRET_KEY = config("SECRET_KEY", default="secret-key-!!!")
+ALLOWED_HOSTS = (
+    ["*"]
+    if DEBUG
+    else config(
+        "ALLOWED_HOSTS", cast=lambda host: [h.strip() for h in host.split(",") if h]
+    )
+)
 
-SECRET_KEY = 'django-insecure--^$0i-n0hnkdxdfwb)iua5&3hw)sxck2i69=bu7h)_)_z!n^nl'
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-# Application
-INSTALLED_APPS = [
-    'jazzmin',  # Third-party App
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Local App
-    "app.account.apps.AccountConfig",
-    "app.core.apps.CoreConfig",
-    "app.post.apps.PostConfig",
-    # Third-party App
-    'ckeditor',
-
+AUTH_PASSWORD_VALIDATORS = [
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'app.core.middlewares.LoginRequiredMiddleware',
 
 ]
+if not DEBUG:
+    MIDDLEWARE.append('apps.core.middlewares.LoginRequiredMiddleware')
 
-ROOT_URLCONF = 'config.urls'
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'app.account.authenticate.EmailAuthBackend',
+]
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'template'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "template"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
-
-WSGI_APPLICATION = 'config.wsgi.application'
-
-# Configures the default database connection to use PostgreSQL.
-# Specifies the database name, username, password, host, and port.
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'socialmediadb',
-        'USER': 'pedram',
-        'PASSWORD': 'pedram@karimi',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
-    }
-}
-
-# Configures the default cache backend to use Redis.
-# Specifies the location of the Redis server (in this case, localhost on port 6379).
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
-    }
-}
-
 # Configures the default template engine to use Django's built-in template engine.
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'apps.account.users_auth.authenticate.JWTAuthentication',
+    ]
+}
 CKEDITOR_CONFIGS = {
     'default': {
         'toolbar': 'full',
@@ -90,48 +79,108 @@ CKEDITOR_CONFIGS = {
         'width': 500,
     },
 }
+# Applications
+APPLICATIONS = ["account", "post", "core"]
 
-SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
+# Serving
+STATIC_URL = "storage/static/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "storage/media"
 
-# Password validation
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
-]
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'app.account.authenticate.EmailAuthBackend',
-]
+# TOKEN Handling
+JWT_AUTH_ACCESS_TOKEN_LIFETIME = timedelta(minutes=20)
+JWT_AUTH_REFRESH_TOKEN_LIFETIME = timedelta(days=30)
+JWT_AUTH_ENCRYPT_KEY = b'32 bytes'
+JWT_AUTH_GET_USER_BY_ACCESS_TOKEN = True
+JWT_AUTH_CACHE_USING = True
 
-# Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Asia/Tehran'
-USE_I18N = True
-USE_TZ = True
+# AWS S3 Configuration
+DEFAULT_FILE_STORAGE = config('DEFAULT_FILE_STORAGE')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = config('AWS_S3_FILE_OVERWRITE', cast=bool, default=False)
+AWS_SERVICE_NAME = config('AWS_SERVICE_NAME', default='s3')
+AWS_LOCAL_STORAGE = f"{BASE_DIR}/storage/aws/"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Logging
+LOG_FILE_PATH = config("LOG_FILE_PATH")
 
-LOGIN_REDIRECT_URL = "/"
-AUTH_USER_MODEL = 'account.User'
+# Cache Handling
+REDIS_URL = f"redis://{config('REDIS_HOST')}:{config('REDIS_PORT')}"
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": REDIS_URL,
+    }
+}
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Mode Handling:
+if DEBUG:
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    INSTALLED_APPS = [
+        "jazzmin",  # Third-party
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        # Third-party
+        "ckeditor",
+        # Application
+        *list(map(lambda app: f"app.{app}", APPLICATIONS)),
+    ]
+    STATICFILES_DIRS = [BASE_DIR / "storage/static"]
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": config("DB_NAME"),
+            "USER": config("DB_USER"),
+            "PASSWORD": config("DB_PASSWORD"),
+            "HOST": config("DB_HOST"),
+            "PORT": config("DB_PORT"),
+        }
+    }
 
-LOG_FILE_PATH = 'app.core.info.log'
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+            "LOCATION": BASE_DIR / "utility/cache",
+        }
+    }
 
-# Configures email settings for sending emails via SMTP using Gmail.
-# Specifies the SMTP server host, port, username, password, and TLS settings.
-# Defines the default sender email address.
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'pedram.9060@gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'kzkq zrty ryjz stii'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = 'Tiny Instagram'
+    EMAIL_BACKEND = config("DEBUG_EMAIL_BACKEND")
+    EMAIL_USE_TLS = config("DEBUG_EMAIL_USE_TLS", cast=bool, default=True)
+    EMAIL_USE_SSL = config("DEBUG_EMAIL_USE_SSL", cast=bool, default=False)
+    EMAIL_HOST = config("DEBUG_EMAIL_HOST")
+    EMAIL_PORT = config("DEBUG_EMAIL_PORT")
+    EMAIL_HOST_USER = config("DEBUG_EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = config("DEBUG_EMAIL_HOST_PASSWORD")
+    DEFAULT_FROM_EMAIL = config("DEBUG_DEFAULT_FROM_EMAIL")
+
+
+else:
+    CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS")
+    STATIC_ROOT = BASE_DIR / "storage/static/"
+    INSTALLED_APPS = [
+        "jazzmin",  # Third-party
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        # Third-party
+        "ckeditor",
+        # Application
+        *list(map(lambda app: f"apps.{app}", APPLICATIONS)),
+    ]
+
+    SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
+
+    EMAIL_BACKEND = config("EMAIL_BACKEND")
+    EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
+    EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)
+    EMAIL_HOST = config("EMAIL_HOST")
+    EMAIL_PORT = config("EMAIL_PORT")
+    EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+    DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
